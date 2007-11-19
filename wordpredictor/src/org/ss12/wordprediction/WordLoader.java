9572b8c6b1016c2d8d9439bbd12c245db4d50142
wordpredictteam <wordpredictteam@880ff09e-672d-11de-89a9-0b20f3e2dceb>
2007-11-19 02:33:22 +0000

    added javadoc

--- wordpredictor/src/org/ss12/wordprediction/WordLoader.java
+++ wordpredictor/src/org/ss12/wordprediction/WordLoader.java
@@ -1,4 +1,5 @@
 package org.ss12.wordprediction;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,124 +18,110 @@ import java.util.Map.Entry;
 public class WordLoader {
 	Integer min_freq;
 	Set<Entry<String, Integer>> set;
-	TreeMap <String, Integer> tm = new TreeMap <String, Integer>();
-	
-	public WordLoader(Integer minimumFrequency)
-	{
-		min_freq = minimumFrequency;	// Initialize the frequency to zero
+	TreeMap<String, Integer> tm = new TreeMap<String, Integer>();
+
+	public WordLoader(Integer minimumFrequency) {
+		min_freq = minimumFrequency; // Initialize the frequency to zero
 	}
-	
+
 	/**
 	 * Loads the given file containing word-frequency pairs.
 	 * 
-	 * @param file the file of word-frequency pairs
+	 * @param file
+	 *            the file of word-frequency pairs
 	 */
-	public void loadFrequenciess(File file) throws IOException 
-	{
-		FileReader	objReader = new FileReader(file);
-		BufferedReader objBuffReader = new BufferedReader (objReader);
-		
-		while(true)
-		{
+	public void loadFrequenciess(File file) throws IOException {
+		FileReader objReader = new FileReader(file);
+		BufferedReader objBuffReader = new BufferedReader(objReader);
+
+		while (true) {
 			String line = objBuffReader.readLine();
-			if(line!= null)
-			{	
+			if (line != null) {
 
 				String[] items = line.split(" ");
 				if (items.length != 2) {
 					continue;
 				}
-				
-				
-				String word="";
+
+				String word = "";
 				Integer value;
-				for(int i=0;i<items.length;i++){
-					if(items[i].length()>0)
-					{
-						if(word.equals(""))
-						{
-							word=items[i];
-						}
-						else
-						{
-							value=Integer.parseInt(items[i]);
+				for (int i = 0; i < items.length; i++) {
+					if (items[i].length() > 0) {
+						if (word.equals("")) {
+							word = items[i];
+						} else {
+							value = Integer.parseInt(items[i]);
 							tm.put(word.toLowerCase(), value);
 							break;
 						}
 					}
 				}
-			}
-			else
-			{
+			} else {
 				break;
 			}
 			set = tm.entrySet();
 		}
 	}
-	
+
 	/**
 	 * Loads the given file containing only words.
 	 * 
-	 * @param file the file of words
-	 * @throws IOException 
-	 * @throws IOException 
+	 * @param file
+	 *            the file of words
+	 * @throws IOException
+	 *             if the dictionary cannot be loaded
 	 */
-	public void loadDictionary(File file) throws IOException  
-	{
+	public void loadDictionary(File file) throws IOException {
 		FileReader objReader = new FileReader(file);
-		BufferedReader objBuffReader = new BufferedReader (objReader);		
-		
-		while(true)
-		{
+		BufferedReader objBuffReader = new BufferedReader(objReader);
+
+		while (true) {
 			String word = objBuffReader.readLine();
-			if(word!= null)
-			{	
-				tm.put(word,new Integer(min_freq));
-			}
-			else
-			{
+			if (word != null) {
+				tm.put(word, new Integer(min_freq));
+			} else {
 				break;
 			}
 			set = tm.entrySet();
 		}
 	}
-	public TreeMap<String, Integer> loadNgram(File file)
-	{
-		TreeMap<String,Integer> retval = new TreeMap<String,Integer>();
-		try{
+
+	/**
+	 * Deserializes the map from n-grams to their associated frequencies.
+	 * 
+	 * @param file the file to deserialize
+	 * @return the n-gram to frequency map
+	 */
+	public TreeMap<String, Integer> loadNgram(File file) {
+		TreeMap<String, Integer> retval = new TreeMap<String, Integer>();
+		try {
 			FileInputStream objReader = new FileInputStream(file);
 			ObjectInputStream in = new ObjectInputStream(objReader);
-			retval = (TreeMap<String, Integer>)in.readObject();
+			retval = (TreeMap<String, Integer>) in.readObject();
 			in.close();
-		}
-		catch(IOException e){
-			System.out.println("File not found, creating empty Ngram entry instead");
-		}
-		catch(ClassNotFoundException e){
+		} catch (IOException e) {
+			System.out
+					.println("File not found, creating empty Ngram entry instead");
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		System.out.println("Contains and:"+retval.containsKey("and"));
-		System.out.println("Contains and histories:"+retval.containsKey("and histories"));
-		System.out.println("Contains about such matters:"+retval.containsKey("about such matters"));
+		System.out.println("Contains and:" + retval.containsKey("and"));
+		System.out.println("Contains and histories:"
+				+ retval.containsKey("and histories"));
+		System.out.println("Contains about such matters:"
+				+ retval.containsKey("about such matters"));
 		return retval;
 	}
-	
-	/**
-	 * Display entries of the TreeMap.
-	 * 
-	 * @param 
-	 */
-	
-	public void display()
-	{
-		for (Map.Entry<String, Integer> me : set) 
-		{
-		      System.out.print(me.getKey() + ": ");
-		      System.out.println(me.getValue());    
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (Map.Entry<String, Integer> me : set) {
+			sb.append(me.getKey() + ": ");
+			sb.append(me.getValue() + ". ");
 		}
-		System.out.println();
+		return sb.toString();
 	}
-	
+
 	/**
 	 * Returns all loaded words as an array of word-frequency pairs.
 	 * 
--- wordpredictor/src/org/ss12/wordprediction/WordReader.java
+++ wordpredictor/src/org/ss12/wordprediction/WordReader.java
@@ -2,73 +2,80 @@ package org.ss12.wordprediction;
 
 import org.ss12.wordprediction.model.PredictionModel;
 
-public class WordReader
-{
+/**
+ * Accepts text and decomposes it into unigrams, bigrams, and tribrams.
+ */
+public class WordReader {
 	private String prePreWord;
 	private String preWord;
 	private final PredictionModel wp;
 	boolean endSentence;
-	public WordReader(PredictionModel wp)
-	{
+
+	/**
+	 * Creates a new word reader, and adds unigrams, bigrams, and trigrams to
+	 * the given prediction model.
+	 * 
+	 * @param wp
+	 *            prediction model to populate
+	 */
+	public WordReader(PredictionModel wp) {
 		this.wp = wp;
 		prePreWord = null;
 		preWord = null;
-		endSentence=false;
+		endSentence = false;
 	}
-	
-	public void nextWords(String[] nw)
-	{	
-		for(int i = 0; i < nw.length; i++)
-		{	
-			endSentence=false;
-			nw[i]=nw[i].toLowerCase();
-			if((nw[i]=wordify(nw[i])).equals("")){
-				prePreWord = preWord= null;
+
+	/**
+	 * Extracts unigrams, bigrams, and trigrams from the given sequence of words
+	 * and updates the prediction model.
+	 * 
+	 * @param words
+	 *            the words
+	 */
+	public void nextWords(String[] words) {
+		for (int i = 0; i < words.length; i++) {
+			endSentence = false;
+			words[i] = words[i].toLowerCase();
+			if ((words[i] = wordify(words[i])).equals("")) {
+				prePreWord = preWord = null;
 				continue;
 			}
-			System.out.print(nw[i]+" ");
-//			System.out.println(wp);
-			wp.addUnigram(nw[i]);
-			
-			if(preWord != null)
-				wp.addBigram(preWord, nw[i]);
-			
-			if(prePreWord != null && preWord != null)
-				wp.addTrigram(prePreWord, preWord, nw[i]);
-			
-			if(!endSentence){
+			System.out.print(words[i] + " ");
+			// System.out.println(wp);
+			wp.addUnigram(words[i]);
+
+			if (preWord != null)
+				wp.addBigram(preWord, words[i]);
+
+			if (prePreWord != null && preWord != null)
+				wp.addTrigram(prePreWord, preWord, words[i]);
+
+			if (!endSentence) {
 				prePreWord = preWord;
-				preWord = nw[i];
-			}
-			else{
-				prePreWord=preWord=null;
+				preWord = words[i];
+			} else {
+				prePreWord = preWord = null;
 				System.out.println();
 			}
 		}
 	}
-	public String wordify(String word){
+
+	private String wordify(String word) {
 		word.trim();
-		for(int i=0;i<word.length()-1;i++){
+		for (int i = 0; i < word.length() - 1; i++) {
 			char c = word.charAt(i);
-			if((c>='a' && c<='z')||(c>='A' && c<='Z')||(c=='\'')||(c=='-')){
+			if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '\'')
+					|| (c == '-')) {
 				continue;
-			}
-			else
+			} else
 				return "";
 		}
-		if(word.length()>0){
-			char c = word.charAt(word.length()-1);
-			if(!(
-				(c>='a' && c<='z')
-					||
-				(c>='A' && c<='Z')
-					||
-				(c=='\'')
-					||
-				(c=='-')
-				)){
-				word = word.substring(0, word.length()-1);
-				endSentence=true;
+		if (word.length() > 0) {
+			char c = word.charAt(word.length() - 1);
+			if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+					|| (c == '\'') || (c == '-'))) {
+				word = word.substring(0, word.length() - 1);
+				endSentence = true;
 			}
 		}
 		return word;
