154273994828d3fab2a295c513d535b565053d3c
wordpredictteam <wordpredictteam@880ff09e-672d-11de-89a9-0b20f3e2dceb>
2008-03-30 22:44:30 +0000

    integrate user input strings, and update the grams

--- wordpredictor/src/org/ss12/wordprediction/ui/ConsolePredictionLauncher.java
+++ wordpredictor/src/org/ss12/wordprediction/ui/ConsolePredictionLauncher.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import org.ss12.wordprediction.WordPredictor;
+import org.ss12.wordprediction.WordReader;
 import org.ss12.wordprediction.model.PredictionModel;
 
 public class ConsolePredictionLauncher {
@@ -13,12 +14,15 @@ public class ConsolePredictionLauncher {
 	 */
 	public static void main(String[] args) {
 		PredictionModel predictor = new WordPredictor();
+		WordReader wordReader = new WordReader(predictor);
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		while(true){
 			try {
 				System.out.print("Enter a prediction query (e.g. 'experi'): ");
 				String input = in.readLine();
 				String[] results = predictor.getSuggestionsGramBased(predictor.processString(input), 5);
+				// update unigram, bigram and trigram according to user input string
+				wordReader.nextWords(predictor.processString(input));
 				for(String r:results) System.out.println(r);
 			} catch (IOException e) {
 				e.printStackTrace();
