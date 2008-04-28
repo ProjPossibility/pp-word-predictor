2c07ff41385278d795843da345fc235af6336299
wordpredictteam <wordpredictteam@880ff09e-672d-11de-89a9-0b20f3e2dceb>
2008-04-28 06:30:25 +0000

    add comments

--- wordpredictor/testsrc/org/ss12/wordprediction/newcore/CustomLexiconTest.java
+++ wordpredictor/testsrc/org/ss12/wordprediction/newcore/CustomLexiconTest.java
@@ -24,32 +24,44 @@ public abstract class CustomLexiconTest extends TestCase {
   protected void tearDown() {
     customLexicon.close();
   }
-  
+
+  /**
+   * A subclass implements this method, returning the implementation of
+   * {@link CustomLexicon} to test.
+   * 
+   * @return the lexicon to test
+   */
   protected abstract CustomLexicon<MockAnnotation> getCustomLexicon();
 
   public void testAddUnigrams() {
     MockAnnotation annotation1 = new MockAnnotation("a");
     MockAnnotation annotation2 = new MockAnnotation("aaa");
     MockAnnotation annotation3 = new MockAnnotation("b");
+
+    // Assert that custom lexicon is empty at start.
     assertEquals(Collections.emptyList(), toList(customLexicon.getUnigrams("")));
     assertEquals(Collections.emptyList(),
         toList(customLexicon.getUnigrams("a")));
 
+    // Assert that can find first annotated word given "" or "a".
     customLexicon.addUnigram(annotation1.getWord());
     assertEquals(Arrays.asList(annotation1), toList(customLexicon
         .getUnigrams("")));
     assertEquals(Arrays.asList(annotation1), toList(customLexicon
         .getUnigrams("a")));
 
+    // Assert that can find first two annotated words given "" or "a".
     customLexicon.addUnigram(annotation2.getWord());
     assertEquals(Arrays.asList(annotation1, annotation2), toList(customLexicon
         .getUnigrams("")));
     assertEquals(Arrays.asList(annotation1, annotation2), toList(customLexicon
         .getUnigrams("a")));
 
+    // Assert that can find all annotated words given "".
     customLexicon.addUnigram(annotation3.getWord());
     assertEquals(Arrays.asList(annotation1, annotation2, annotation3),
         toList(customLexicon.getUnigrams("")));
+    // Assert that only find first two annotated words given "a".
     assertEquals(Arrays.asList(annotation1, annotation2), toList(customLexicon
         .getUnigrams("a")));
   }
@@ -60,17 +72,20 @@ public abstract class CustomLexiconTest extends TestCase {
     customLexicon.addUnigram(word1);
     customLexicon.addUnigram(word2);
 
+    // Assert that both words have not been updated by default.
     List<MockAnnotation> annotations = toList(customLexicon.getUnigrams(""));
     assertEquals(2, annotations.size());
     assertAnnotation(annotations.get(0), word1, false);
     assertAnnotation(annotations.get(1), word2, false);
 
+    // Add the first word again, and assert that its annotation was updated.
     customLexicon.addUnigram(word1);
     annotations = toList(customLexicon.getUnigrams(""));
     assertEquals(2, annotations.size());
     assertAnnotation(annotations.get(0), word1, true);
     assertAnnotation(annotations.get(1), word2, false);
 
+    // Add the second word again, and assert that its annotation was updated.
     customLexicon.addUnigram(word2);
     annotations = toList(customLexicon.getUnigrams(""));
     assertEquals(2, annotations.size());
@@ -78,6 +93,7 @@ public abstract class CustomLexiconTest extends TestCase {
     assertAnnotation(annotations.get(1), word2, true);
   }
 
+  // Converts the given iterable of annotations to a list.
   private <T extends AnnotatedWord> List<T> toList(Iterable<T> annotations) {
     List<T> list = new LinkedList<T>();
     for (T annotation : annotations) {
@@ -85,8 +101,11 @@ public abstract class CustomLexiconTest extends TestCase {
     }
     return list;
   }
-  
-  private void assertAnnotation(MockAnnotation annotation, String word, boolean updated) {
+
+  // Assert that the annotation has the given word, and whether it was updated
+  // if expected.
+  private void assertAnnotation(MockAnnotation annotation, String word,
+      boolean updated) {
     assertEquals(word, annotation.getWord());
     assertEquals(updated, annotation.resetUpdate());
   }
