76d4023d33802459f4360aad4377fe05fb0c5b0f
wordpredictteam <wordpredictteam@880ff09e-672d-11de-89a9-0b20f3e2dceb>
2008-04-30 08:12:58 +0000

    stealing code out of TreeMapWordPredictor to make it work. This should be refactored eventually.

--- wordpredictor/src/org/ss12/wordprediction/servlet/NewWordPredictorAdapter.java
+++ wordpredictor/src/org/ss12/wordprediction/servlet/NewWordPredictorAdapter.java
@@ -37,7 +37,7 @@ public class NewWordPredictorAdapter implements WordPredictor {
   }
 
   public String[] getSuggestionsGramBased(String[] context, int numOfSuggestions) {
-    if (context.length == 0) {
+    if ((context == null) || (context.length == 0)) {
       return new String[0];
     }
     return wp.getPredictions(PredictionRequest.from(context[context.length - 1],
@@ -48,9 +48,48 @@ public class NewWordPredictorAdapter implements WordPredictor {
     // silently do nothing
     return 0;
   }
+  
+  public String wordify(String word) {
+    word.trim();
+    for (int i = 0; i < word.length() - 1; i++) {
+      char c = word.charAt(i);
+      if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '\'')
+          || (c == '-')) {
+        continue;
+      } else
+        return "";
+    }
+    if (word.length() > 0) {
+      char c = word.charAt(word.length() - 1);
+      if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+          || (c == '\'') || (c == '-'))) {
+        word = word.substring(0, word.length() - 1);
+      }
+    }
+    return word;
+  }
 
   public String[] processString(String input) {
-    // silently do nothing
-    return null;
+    input=input.toLowerCase();
+    int startPoint=Math.max(input.lastIndexOf('.'), Math.max(input.lastIndexOf('?'),input.lastIndexOf('!')))+1;
+    if(startPoint>=input.length())
+      return new String[]{""};
+    else if(startPoint>0)
+      input = input.substring(startPoint);
+    String temp[] = new String[3];
+    int[] ind = new int[3];
+    ind[0] = input.lastIndexOf(' ');
+    temp[0] = input.substring(ind[0]+1);
+    int i;
+    for(i=1; ind[i-1]>0 && i<3; i++){
+      ind[i] = input.lastIndexOf(' ', ind[i-1]-1);
+      temp[i] = input.substring(ind[i]+1, ind[i-1]);
+    }
+    String word[] = new String[i];
+    for(int j=0;j<i;j++){
+      temp[i-j-1] = wordify(temp[i-j-1]);
+      word[j] = temp[i-j-1].trim();
+    }
+    return word;
   }
 }
